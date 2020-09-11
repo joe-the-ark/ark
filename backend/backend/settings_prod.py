f03379e5afc29d072614766145b45c5814323f24
LehaoLin <lehaolin@link.cuhk.edu.cn>
2020-09-11 20:59:45 +0800

    prepare upload to the server

---
+++ backend/backend/settings_prod.py
@@ -0,0 +1,8 @@
+from .settings import *
+
+DEBUG = True
+LOCAL = False
+
+
+STATIC_ROOT = '/var/ark2020/static/'
+MEDIA_ROOT = '/var/ark2020/media/'
---
+++ build.sh
@@ -0,0 +1,11 @@
+# git fetch origin new
+# git merge origin/master new
+git pull
+
+# pip3 install -r requirements.txt
+
+cd backend
+python3 manage.py migrate --settings=backend.settings_prod
+python3 manage.py collectstatic --no-input --settings=backend.settings_prod
+
+supervisorctl restart ark2020
