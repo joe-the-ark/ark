8a91e114c936fc410ceac4a83ab32a7118a39d4d
LehaoLin <lehaolin@link.cuhk.edu.cn>
2020-11-14 21:56:08 +0800

    add: mysql

--- backend/backend/settings_prod.py
+++ backend/backend/settings_prod.py
@@ -1,7 +1,20 @@
 from .settings import *
 
+import pymysql
+
+pymysql.install_as_MySQLdb()
+
 DEBUG = True
 LOCAL = False
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ark2020',
+        'USER': 'root',
+        'PASSWORD': 'Joe!',
+    }
+}
 
 
 STATIC_ROOT = '/var/ark2020/static/'
