201d4829013b8749373a2bd2bc5e803e6a622acb
LehaoLin <lehaolin@link.cuhk.edu.cn>
2021-03-14 22:27:33 +0800

    delete some useless print

--- backend/arkserver/decorators.py
+++ backend/arkserver/decorators.py
@@ -8,12 +8,12 @@ from django.shortcuts import redirect
 
 def _check_user(request):
     from .models import Player, Game
-    print('request', request)
+    # print('request', request)
     if 'uid' not in request.session:
         return 
 
     user = Player.objects.filter(id=request.session['uid']).first()
-    print('user',user)
+    # print('user',user)
     
     return user
 
@@ -30,7 +30,7 @@ def user_required(func):
 
         request.user = user
         # request.game = Game.objects.filter(link=request.session['link']).first()
-        print('link', request.session['link'])
+        # print('link', request.session['link'])
 
         return func(request, user=user, *args, **kwargs)
 
@@ -41,7 +41,7 @@ def after_waitingroom(func):
     from .models import Player, WaitingRoomMember
     def view(request, *args, **kwargs):
         user = _check_user(request)
-        print('user-valid', user.valid)
+        # print('user-valid', user.valid)
         if not user.valid:
             return redirect(f'/')
         return func(request, *args, **kwargs)
